275c19211ca6a9f892290fdec9a3eff9e9862732
jang-namu <ironhee8005@gmail.com>
2025-02-15 01:55:05 +0900

    티켓을 구현하라

--- src/main/java/com/namu/object/_01_ticket/Ticket.java
+++ src/main/java/com/namu/object/_01_ticket/Ticket.java
@@ -1,2 +1,13 @@
-package com.namu.object._01_ticket;public class Ticket {
+package com.namu.object._01_ticket;
+
+public class Ticket {
+    private Long fee;
+
+    public Long getFee() {
+        return fee;
+    }
+
+    public Ticket(Long fee) {
+        this.fee = fee;
+    }
 }
