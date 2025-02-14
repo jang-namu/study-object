f9503cce6ac62c801a0a8d694426e7dccf87ed95
jang-namu <ironhee8005@gmail.com>
2025-02-15 01:57:08 +0900

    극장을 구현하라

--- src/main/java/com/namu/object/_01_ticket/Theater.java
+++ src/main/java/com/namu/object/_01_ticket/Theater.java
@@ -1,2 +1,13 @@
-package com.namu.object._01_ticket;public class Theater {
+package com.namu.object._01_ticket;
+
+public class Theater {
+    private TicketSeller ticketSeller;
+
+    public Theater(TicketSeller ticketSeller) {
+        this.ticketSeller = ticketSeller;
+    }
+
+    public void enter(Audience audience) {
+        ticketSeller.sellTo(audience);
+    }
 }
