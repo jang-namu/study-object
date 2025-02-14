d9114002ab8bb3179095ada89ef04a4763a13ca5
jang-namu <ironhee8005@gmail.com>
2025-02-15 01:56:07 +0900

    관람객을 구현하라

--- src/main/java/com/namu/object/_01_ticket/Audience.java
+++ src/main/java/com/namu/object/_01_ticket/Audience.java
@@ -1,2 +1,13 @@
-package com.namu.object._01_ticket;public class Audience {
+package com.namu.object._01_ticket;
+
+public class Audience {
+    private Bag bag;
+
+    public Audience(Bag bag) {
+        this.bag = bag;
+    }
+
+    public Long buy(Ticket ticket) {
+        return bag.hold(ticket);
+    }
 }
