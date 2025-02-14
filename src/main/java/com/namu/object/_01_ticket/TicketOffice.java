7ac1b227fdd69bad365f65700a32249cdbea2eac
jang-namu <ironhee8005@gmail.com>
2025-02-15 01:56:38 +0900

    매표소를 구현하라

--- src/main/java/com/namu/object/_01_ticket/TicketOffice.java
+++ src/main/java/com/namu/object/_01_ticket/TicketOffice.java
@@ -1,2 +1,39 @@
-package com.namu.object._01_ticket;public class TicketOffice {
+package com.namu.object._01_ticket;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class TicketOffice {
+    private Long amount;
+    private List<Ticket> tickets = new ArrayList<>();
+
+    public TicketOffice(Long amount, Ticket ... tickets) {
+        this.amount = amount;
+        this.tickets.addAll(Arrays.asList(tickets));
+    }
+
+    public Long getAmount() {
+        return amount;
+    }
+
+    public int getTicketsCount() {
+        return tickets.size();
+    }
+
+    public void sellTicketTo(Audience audience) {
+        plusAmount(audience.buy(getTicket()));
+    }
+
+    private Ticket getTicket() {
+        return tickets.remove(0);
+    }
+
+    private void minusAmount(Long amount) {
+        this.amount -= amount;
+    }
+
+    private void plusAmount(Long amount) {
+        this.amount += amount;
+    }
 }
