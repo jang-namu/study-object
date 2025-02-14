9d188f541d1d8c171f2f8d649d2301bd015dd5b4
jang-namu <ironhee8005@gmail.com>
2025-02-15 01:58:59 +0900

    티켓 구매/교환을 위한 Bag.hold 메소드를 테스트하라

--- src/test/java/com/namu/object/_01_ticket/BagTest.java
+++ src/test/java/com/namu/object/_01_ticket/BagTest.java
@@ -1,40 +1,41 @@
-package _01_ticket;
+package com.namu.object._01_ticket;
 
-import com.namu.object._01_ticket.Bag;
-import com.namu.object._01_ticket.Invitation;
-import com.namu.object._01_ticket.Ticket;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+@DisplayName("가방 테스트")
 public class BagTest {
+    private static final Long TICKET_PRICE = 2000L;
+    private static final Long INITIAL_AMOUNT = 10000L;
 
-    @DisplayName("초대장을 소지한 경우")
     @Test
-    void changeTicket() {
+    @DisplayName("초대장이 있으면 값을 지불하지 않고 티켓을 받는다")
+    void shouldExchangeTicketForFree() {
         // given
-        Bag bag = new Bag(new Invitation(), 2000L);
-        Ticket ticket = new Ticket(2000L);
+        Bag bag = new Bag(new Invitation(), INITIAL_AMOUNT);
+        Ticket ticket = new Ticket(TICKET_PRICE);
 
         // when
         bag.hold(ticket);
 
         // then
-        assertThat(bag.getTicket()).isTrue();
-        assertThat(bag.getAmount()).isEqual(2000L);
+//        assertThat(bag.hasTicket()).isTrue();
+//        assertThat(bag.getAmount()).isEqual(INITIAL_AMOUNT);
     }
 
-    @DisplayName("초대장을 소지하지 않은 경우")
     @Test
-    void buyTicket() {
+    @DisplayName("초대장이 없으면 값을 지불하고 티켓을 구매한다")
+    void shouldPayForTicket() {
         // given
-        Bag bag = new Bag(2000L);
-        Ticket ticket = new Ticket(2000L);
+        Bag bag = new Bag(INITIAL_AMOUNT);
+        Ticket ticket = new Ticket(TICKET_PRICE);
 
         // when
         bag.hold(ticket);
 
         // then
-        assertThat(bag.getTicket()).isTrue();
-        assertThat(bag.getAmount()).isEqual(0L);
+//        assertThat(bag.hasTicket()).isTrue();
+//        assertThat(bag.getAmount()).isEqual(INITIAL_AMOUNT - TICKET_PRICE);
     }
 }
