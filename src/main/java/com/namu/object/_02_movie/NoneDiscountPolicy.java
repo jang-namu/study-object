c4af54a11bba394888e28fdbb40a2bc6e95c9741
jang-namu <ironhee8005@gmail.com>
2025-02-16 20:15:30 +0900

    fix: extends -> implements 수정

--- src/main/java/com/namu/object/_02_movie/NoneDiscountPolicy.java
+++ src/main/java/com/namu/object/_02_movie/NoneDiscountPolicy.java
@@ -1,6 +1,6 @@
 package com.namu.object._02_movie;
 
-public class NoneDiscountPolicy extends DiscountPolicy {
+public class NoneDiscountPolicy implements DiscountPolicy {
     @Override
     public Money calculateDiscountAmount(Screening screening) {
         return Money.ZERO;
