54a97395cfb1f2089468272226acb5ccb230d97a
jang-namu <ironhee8005@gmail.com>
2025-02-16 18:29:19 +0900

    refactor: 할인 정책이 없는 경우를 개선하라

---
+++ src/main/java/com/namu/object/_02_movie/NoneDiscountPolicy.java
@@ -0,0 +1,8 @@
+package com.namu.object._02_movie;
+
+public class NoneDiscountPolicy extends DiscountPolicy {
+    @Override
+    protected Money getDiscountAmount(Screening screening) {
+        return Money.ZERO;
+    }
+}
