4fb945deff113728401bd5d0f3dd503127dc3e2c
jang-namu <ironhee8005@gmail.com>
2025-02-16 18:34:03 +0900

    NoneDiscountPolicy와 DiscountPolicy의 개념적 결합을 분리한다

--- src/main/java/com/namu/object/_02_movie/AmountDiscountPolicy.java
+++ src/main/java/com/namu/object/_02_movie/AmountDiscountPolicy.java
@@ -1,6 +1,6 @@
 package com.namu.object._02_movie;
 
-public class AmountDiscountPolicy extends DiscountPolicy {
+public class AmountDiscountPolicy extends DefaultDiscountPolicy {
     private Money discountAmount;
 
     public AmountDiscountPolicy(Money discountAmount, DiscountCondition... conditions) {
