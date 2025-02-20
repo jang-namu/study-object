ecb4943c32f003c93704f00a7a011512f4a2caa2
jang-namu <ironhee8005@gmail.com>
2025-02-21 02:50:52 +0900

    데이터 중심 설계 개선하기(2) 영화 요금 계산 로직을 재배치한다

--- src/test/java/com/namu/object/_03_movie/Movie.java
+++ src/test/java/com/namu/object/_03_movie/Movie.java
@@ -1,12 +1,9 @@
 package com.namu.object._03_movie;
 
 import com.namu.object._02_movie.Money;
+import java.time.LocalDateTime;
 import java.util.List;
-import lombok.Getter;
-import lombok.Setter;
 
-@Getter
-@Setter
 public class Movie {
     private String title;
     private Duration runningTime;
@@ -16,4 +13,43 @@ public class Movie {
     private Money discountAmount;
     private double discountPercent;
 
+    public MovieType getMovieType() {
+        return movieType;
+    }
+
+    public Money calculateAmountDiscountFee() {
+        if (movieType != MovieType.AMOUNT_DISCOUNT) {
+            throw new IllegalArgumentException();
+        }
+        return fee.minus(discountAmount);
+    }
+
+    public Money calculatePercentDiscountFee() {
+        if (movieType != MovieType.PERCENT_DISCOUNT) {
+            throw new IllegalArgumentException();
+        }
+        return fee.minus(fee.times(discountPercent));
+    }
+
+    public Money calculateNoneDiscountFee() {
+        if (movieType != MovieType.NONE_DISCOUNT) {
+            throw new IllegalArgumentException();
+        }
+        return fee;
+    }
+
+    public boolean isDiscountable(LocalDateTime whenScreened, int sequence) {
+        for (DiscountCondition condition : discountConditions) {
+            if (condition.getType() == DiscountConditionType.PERIOD) {
+                if (condition.isDiscountable(whenScreened.getDayOfWeek(), whenScreened.toLocalTime())) {
+                    return true;
+                }
+            } else {
+                if (condition.isDiscountable(sequence)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
 }
