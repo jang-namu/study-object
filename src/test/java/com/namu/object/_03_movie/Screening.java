cd4ed068f2c90d5e177ac84e241ffac2d91bae5d
jang-namu <ironhee8005@gmail.com>
2025-02-20 16:13:00 +0900

    데이터 중심, 나머지 데이터 클래스를 구현한다

--- src/test/java/com/namu/object/_03_movie/Screening.java
+++ src/test/java/com/namu/object/_03_movie/Screening.java
@@ -1,4 +1,13 @@
 package com.namu.object._03_movie;
 
+import java.time.LocalDateTime;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class Screening {
+    private Movie movie;
+    private int sequence;
+    private LocalDateTime whenScreened;
 }
