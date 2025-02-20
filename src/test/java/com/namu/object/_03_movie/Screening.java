79ee6cc3530b19c5153bdf1ea92daa0d33046466
jang-namu <ironhee8005@gmail.com>
2025-02-21 02:51:22 +0900

    데이터 중심 설계 개선하기(3) 상영 요금 계산 로직을 재배치한다

--- src/test/java/com/namu/object/_03_movie/Screening.java
+++ src/test/java/com/namu/object/_03_movie/Screening.java
@@ -1,13 +1,27 @@
 package com.namu.object._03_movie;
 
+import com.namu.object._02_movie.Money;
 import java.time.LocalDateTime;
-import lombok.Getter;
-import lombok.Setter;
 
-@Getter
-@Setter
 public class Screening {
     private Movie movie;
     private int sequence;
     private LocalDateTime whenScreened;
+
+    public Money calculateFee(int audienceCount) {
+        switch (movie.getMovieType()) {
+            case AMOUNT_DISCOUNT:
+                if (movie.isDiscountable(whenScreened, sequence)) {
+                    return movie.calculateAmountDiscountFee().times(audienceCount);
+                }
+                break;
+            case PERCENT_DISCOUNT:
+                if (movie.isDiscountable(whenScreened, sequence)) {
+                    return movie.calculatePercentDiscountFee().times(audienceCount);
+                }
+            case NONE_DISCOUNT:
+                return movie.calculateNoneDiscountFee().times(audienceCount);
+        }
+        return movie.calculateNoneDiscountFee().times(audienceCount);
+    }
 }
