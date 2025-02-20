a73191f632bff64d8ca2dd594078d2b7b663bdf8
jang-namu <ironhee8005@gmail.com>
2025-02-20 16:12:36 +0900

    데이터 중심, 할인 조건을 구현한다

--- src/test/java/com/namu/object/_03_movie/DiscountCondition.java
+++ src/test/java/com/namu/object/_03_movie/DiscountCondition.java
@@ -1,2 +1,20 @@
-package com.namu.object._03_movie;public class DiscountCondition {
+package com.namu.object._03_movie;
+
+import java.time.DayOfWeek;
+import java.time.LocalTime;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class DiscountCondition {
+    private DiscountConditionType type;
+
+    // 순번 조건에만 사용
+    private int sequence;
+
+    // 기간 조건에만 사용
+    private DayOfWeek dayOfWeek;
+    private LocalTime startTime;
+    private LocalTime endTime;
 }
