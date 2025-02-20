34b66918c2528f150acfaecc0e74b20cc80ab9ed
jang-namu <ironhee8005@gmail.com>
2025-02-20 16:13:00 +0900

    데이터 중심, 나머지 데이터 클래스를 구현한다

--- src/test/java/com/namu/object/_03_movie/Customer.java
+++ src/test/java/com/namu/object/_03_movie/Customer.java
@@ -1,2 +1,11 @@
-package com.namu.object._03_movie;public class Customer {
+package com.namu.object._03_movie;
+
+public class Customer {
+    private String name;
+    private String id;
+
+    public Customer(String name, String id) {
+        this.name = name;
+        this.id = id;
+    }
 }
--- src/test/java/com/namu/object/_03_movie/Reservation.java
+++ src/test/java/com/namu/object/_03_movie/Reservation.java
@@ -1,2 +1,22 @@
-package com.namu.object._03_movie;public class Reservation {
+package com.namu.object._03_movie;
+
+import com.namu.object._02_movie.Customer;
+import com.namu.object._02_movie.Money;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Reservation {
+    private Customer customer;
+    private Screening screening;
+    private Money fee;
+    private int audienceCount;
+
+    public Reservation(Customer customer, Screening screening, Money fee, int audienceCount) {
+        this.customer = customer;
+        this.screening = screening;
+        this.fee = fee;
+        this.audienceCount = audienceCount;
+    }
 }
