f847f276f8c65abe2e88ee23edb6574d31eeacdc
jang-namu <ironhee8005@gmail.com>
2025-02-20 16:10:13 +0900

    데이터 중심 설계, Movie가 가질 데이터를 구현한다

---
+++ src/test/java/com/namu/object/_03_movie/Movie.java
@@ -0,0 +1,15 @@
+package com.namu.object._03_movie;
+
+import com.namu.object._02_movie.Money;
+import java.util.List;
+
+public class Movie {
+    private String title;
+    private Duration runningTime;
+    private Money fee;
+    private List<DiscountCondition> discountConditions;
+    private MovieType movieType;
+    private Money discountAmount;
+    private double discountPercent;
+
+}
