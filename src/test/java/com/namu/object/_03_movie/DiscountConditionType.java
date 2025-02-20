b138cee8ea9a370724dbbb7cef1a38dd6606ba4f
jang-namu <ironhee8005@gmail.com>
2025-02-20 16:12:36 +0900

    데이터 중심, 할인 조건을 구현한다

--- src/test/java/com/namu/object/_03_movie/DiscountConditionType.java
+++ src/test/java/com/namu/object/_03_movie/DiscountConditionType.java
@@ -1,2 +1,6 @@
-package com.namu.object._03_movie;public class DiscountConditionType {
+package com.namu.object._03_movie;
+
+public enum DiscountConditionType {
+    SEQUENCE, // 순번 조건
+    PERIOD    // 기간 조건
 }
