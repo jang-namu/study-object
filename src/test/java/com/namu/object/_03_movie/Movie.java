85ac6e1821c69452c72ab5b4cafc8b356fac4801
jang-namu <ironhee8005@gmail.com>
2025-02-20 16:11:21 +0900

    캡슐화를 위해 접근자와 지정자를 추가한다

--- src/test/java/com/namu/object/_03_movie/Movie.java
+++ src/test/java/com/namu/object/_03_movie/Movie.java
@@ -2,7 +2,11 @@ package com.namu.object._03_movie;
 
 import com.namu.object._02_movie.Money;
 import java.util.List;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
 public class Movie {
     private String title;
     private Duration runningTime;
