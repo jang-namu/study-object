b3b5edd2b43c53f87efccda7523e7f76cee03c12
jang-namu <ironhee8005@gmail.com>
2025-02-28 03:07:35 +0900

    요금 계산을 위해 필요한 Movie의 데이터를 선택한다

--- src/main/java/com/namu/object/_05_movie/Movie.java
+++ src/main/java/com/namu/object/_05_movie/Movie.java
@@ -1,8 +1,20 @@
 package com.namu.object._05_movie;
 
+import com.namu.object._02_movie.DiscountCondition;
 import com.namu.object._02_movie.Money;
+import com.namu.object._05_movie.MovieType;
+import java.util.List;
 
 public class Movie {
+    private String title;
+    private Duration duration;
+    private Moeny fee;
+    private List<DiscountCondition> discountConditions;
+
+    private MovieType movieType;
+    private Money discountAmount;
+    private double discountPercent;
+
     public Money calculateMovieFee(Screening screening) {
 
     }
