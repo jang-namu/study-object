1f62552f46432b5a7c2ea39fb58444796da512ca
jang-namu <ironhee8005@gmail.com>
2025-02-28 03:14:06 +0900

    영화는 할인조건에 할인 여부 판단을 위임하고 금액을 계산한다

--- src/main/java/com/namu/object/_05_movie/Movie.java
+++ src/main/java/com/namu/object/_05_movie/Movie.java
@@ -8,7 +8,7 @@ import java.util.List;
 public class Movie {
     private String title;
     private Duration duration;
-    private Moeny fee;
+    private Money fee;
     private List<DiscountCondition> discountConditions;
 
     private MovieType movieType;
@@ -16,6 +16,39 @@ public class Movie {
     private double discountPercent;
 
     public Money calculateMovieFee(Screening screening) {
-
+        if (isDiscountable(screening)) {
+            return fee.minus(calculateDiscountAmount());
+        }
+        return fee;
     }
+
+    private boolean isDiscountable(Screening screening) {
+        return discountConditions.stream()
+                .anyMatch(condition -> condition.isSatisfiedBy(screening));
+    }
+
+    private Money calculateDiscountAmount() {
+        switch (movieType) {
+            case AMOUNT_DISCOUNT:
+                return calculateAmountDiscountAmount();
+            case PERCENT_DISCOUNT:
+                return calculatePercentDiscountAmount();
+            case NONE_DISCOUNT:
+                return calculateNoneDiscountAmount();
+        }
+        throw new IllegalStateException();
+    }
+
+    private Money calculateNoneDiscountAmount() {
+        return Money.ZERO;
+    }
+
+    private Money calculatePercentDiscountAmount() {
+        return fee.times(discountPercent);
+    }
+
+    private Money calculateAmountDiscountAmount() {
+        return discountAmount;
+    }
+
 }
