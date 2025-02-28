aa6bf66e2a6561823ec31a999d4b0d2b32da1274
jang-namu <ironhee8005@gmail.com>
2025-02-28 17:27:07 +0900

    할인 요금 계산은 가격 정보를 가진 영화 클래스로 이동한다

---
+++ src/main/java/com/namu/object/_05_grasp/_04_movie_refactor/Movie.java
@@ -0,0 +1,41 @@
+package com.namu.object._05_grasp._04_movie_refactor;
+
+import com.namu.object._02_movie.Money;
+import com.namu.object._04_movie.DiscountCondition;
+import com.namu.object._04_movie.MovieType;
+import java.util.List;
+
+public class Movie {
+    private String title;
+    private Duration runningTime;
+    private Money fee;
+    private List<DiscountCondition> discountConditions;
+    private MovieType movieType;
+    private Money discountAmount;
+    private double discountPercent;
+
+    public Money calculateDiscountAmount() {
+        switch (movieType) {
+            case AMOUNT_DISCOUNT:
+                return calculateAmountDiscountAmount();
+            case PERCENT_DISCOUNT:
+                return calculatePercentDiscountAmount();
+            case NONE_DISCOUNT:
+                return calculateNoneDiscountAmount();
+        }
+        throw new IllegalStateException();
+    }
+
+    private Money calculateAmountDiscountAmount() {
+        return discountAmount;
+    }
+
+    private Money calculatePercentDiscountAmount() {
+        return fee.times(discountPercent);
+    }
+
+    private Money calculateNoneDiscountAmount() {
+        return Money.ZERO;
+    }
+
+}
